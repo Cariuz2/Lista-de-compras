516c963bf31937a5cc87fb38cb2caeeb7f10f842
Gustavo Cariuz <105025153+Cariuz2@users.noreply.github.com>
2026-04-10 14:59:23 -0300

    Add files via upload

---
+++ lista.py
@@ -0,0 +1,54 @@
+import os
+
+print('Lista de compras \n ____________________')
+
+entrada = '1'
+lista = []
+item = ''
+valor = ''
+while True:
+
+    entrada = input('Escolha uma das opções abaixo: \n' \
+    '1. Adicionar itens na lista;\n' \
+    '2. Apagar itens na lista; \n' \
+    '3. Listar os valores da lista;\n' \
+    '4. Encerrar e imprimir a lista.\n')
+
+    entradas_permitidas = '1234'
+
+    if entrada not in entradas_permitidas:
+        print('Número inválido.')
+        continue
+    ########################################################################################################################
+    # ADD
+
+    if entrada == '1':
+        os.system('cls' if os.name == 'nt' else clear)
+        item = input('Nome do item: ')
+        lista.append(item)
+        continue
+
+    # DELETE
+    
+    if entrada == '2':
+        os.system('cls' if os.name == 'nt' else clear)
+        valor = input('Índice do item:')
+        lista.pop(int(valor))
+
+
+    # VIEW
+
+    if entrada == '3':
+        os.system('cls' if os.name == 'nt' else clear)
+        for indice, item in enumerate(lista):
+            print(indice, item)
+        continue
+
+    # STOP
+
+    if entrada == '4':
+        os.system('cls' if os.name == 'nt' else clear)
+        for indice, item in enumerate(lista):
+            print(indice, item)
+        break
+    
